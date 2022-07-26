8edca21a1d24d368d5c89e29380749ee82205a37
Luisibz97 <luis_rab_20@hotmail.com>
2022-07-26 02:24:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+class Estudiante {
+    nombre;
+    asignaturas;
+
+    constructor(nombre, asignaturas) {
+
+        this.nombre      = nombre
+        this.asignaturas = asignaturas
+    }
+    obtenDatos(){
+        return {
+            nombre:this.nombre,
+            asignaturas: this.asignaturas
+        }
+    }
+
+}
+
+const estudiante = new Estudiante('Luis',['Javascript','HTML','CSS']);
+
+console.log(estudiante.obtenDatos())
